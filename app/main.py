349269933844b2bc5b39860e7501048ffcf55c2b
apteryxlabs <matthew@apteryxlabs.com>
2021-07-06 18:15:41 -0400

    it works!

--- app/main.py
+++ app/main.py
@@ -9,13 +9,13 @@ from app.database import add_message, get_messages
 def get_application():
     _app = FastAPI(title=settings.PROJECT_NAME)
 
-    _app.add_middleware(
-        CORSMiddleware,
-        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
+    # _app.add_middleware(
+    #     CORSMiddleware,
+    #     allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+    #     allow_credentials=True,
+    #     allow_methods=["*"],
+    #     allow_headers=["*"],
+    # )
 
     return _app
 
